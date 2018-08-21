512cce819a38e68b1708fee80cceaeba49fe48dc
GeorginaXu <cxu@smith.edu>
2018-08-21 16:15:59 +0800

    added setting page and functions to update the display value and calculation

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by georginaxu on 8/21/18.
+//  Copyright © 2018 georginaxu. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var firstTipOption: UITextField!
+    @IBOutlet weak var secondTipOption: UITextField!
+    @IBOutlet weak var thirdTipOption: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Update the tip values in the text fields.
+        let userDefaults = UserDefaults.standard
+        let firstTip = userDefaults.integer(forKey: "firstTipOption")
+        let secondTip = userDefaults.integer(forKey: "secondTipOption")
+        let thirdTip = userDefaults.integer(forKey: "thirdTipOption")
+        
+        firstTipOption.text = "\(firstTip)"
+        secondTipOption.text = "\(secondTip)"
+        thirdTipOption.text = "\(thirdTip)"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func storeChangedValue(_ sender: Any) {
+        let userDefaults = UserDefaults.standard
+        let firstTip = Int(firstTipOption.text!) ?? 0
+        let secondTip = Int(secondTipOption.text!) ?? 0
+        let thirdTip = Int(thirdTipOption.text!) ?? 0
+        
+        firstTipOption.text = "\(firstTip)"
+        secondTipOption.text = "\(secondTip)"
+        thirdTipOption.text = "\(thirdTip)"
+        
+        userDefaults.set(firstTip, forKey: "firstTipOption")
+        userDefaults.set(secondTip, forKey: "secondTipOption")
+        userDefaults.set(thirdTip, forKey: "thirdTipOption")
+        
+        userDefaults.synchronize()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -21,9 +21,45 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultView: UIView!
     
+    var tipPercentages = [0.18, 0.2, 0.25]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Store initial tip value in user defaults.
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(18, forKey: "firstTipOption")
+        userDefaults.set(20, forKey: "secondTipOption")
+        userDefaults.set(25, forKey: "thirdTipOption")    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let userDefaults = UserDefaults.standard
+        let firstTipOption = userDefaults.integer(forKey: "firstTipOption")
+        let secondTipOption = userDefaults.integer(forKey: "secondTipOption")
+        let thirdTipOption = userDefaults.integer(forKey: "thirdTipOption")
+        
+        selectTipPercentage.setTitle("\(firstTipOption)%", forSegmentAt: 0)
+        selectTipPercentage.setTitle("\(secondTipOption)%", forSegmentAt: 1)
+        selectTipPercentage.setTitle("\(thirdTipOption)%", forSegmentAt: 2)
+        
+        // Update the value in tip percentage list.
+        tipPercentages[0] = Double(firstTipOption) / 100.0
+        tipPercentages[1] = Double(secondTipOption) / 100.0
+        tipPercentages[2] = Double(thirdTipOption) / 100.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,7 +72,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        let tipPercentages = [0.18, 0.2, 0.25]
+        // Retrieve the bill value.
         let bill = Double(billValue.text!) ?? 0
     
         let tip = bill * tipPercentages[selectTipPercentage.selectedSegmentIndex]
